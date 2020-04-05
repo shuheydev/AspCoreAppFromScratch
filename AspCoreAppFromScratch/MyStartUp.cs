5d35565b962d2ddf04d8f504aa19aacb4095ae09
Shuhey <shuhey.dev@gmail.com>
2020-04-05 16:27:13 +0900

    Add project files.

---
+++ AspCoreAppFromScratch/MyStartUp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspCoreAppFromScratch
+{
+    public class MyStartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //Routingの仕組みを使うよ。
+            app.UseRouting();
+
+            //Routingの具体的な指定はこれだよ。
+            app.UseEndpoints(endpoints =>
+            {
+                //ここに指定する
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ AspCoreAppFromScratch/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace AspCoreAppFromScratch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //サーバーインスタンスの設定
+            IHostBuilder hostBuilder = MyCreateHostBuilder(args);
+
+            //サーバーインスタンスの生成、サーバープロセスの起動
+            hostBuilder.Build()
+                       .Run();
+        }
+
+        private static IHostBuilder MyCreateHostBuilder(string[] args)
+        {
+            //MyStartUpクラスの中で色々な設定を行う
+            return Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<MyStartUp>();
+                        });
+        }
+    }
+}
